88e758997a6c3b1a21df772d3e60c371476e7db3
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-20 16:52:55 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,8 +1,8 @@
 
 module Findable
   module ClassMethods
-    def find_by_name(name)
-      self.all.detect{|o| o.name}
-    end
-  end
+    def find_by_name(name)           #reference the collection of all of the instances of a class, 
+      self.all.detect{|o| o.name}    #without specifically referencing class variables 
+  end                                #that are only defined in certain classes
+end
 end
