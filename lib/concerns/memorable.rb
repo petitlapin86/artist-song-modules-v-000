b44f8798993eb14d4249a4c67090e2f3582fab06
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-20 16:31:30 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,2 +1,21 @@
 module Memorable
+
+  def self.reset_all
+  @@songs.clear
+  end
+
+  def self.count
+  self.all.count
+  end
+
+  def self.reset_all
+  self.all.clear #can delete all artists
+  #end
+
+  def self.count
+  @@artists.count #can keep track of/count artists
+  end
+
+
+
 end
