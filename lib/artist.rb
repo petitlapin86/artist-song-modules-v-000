9f3cabdb6b3194ea58f99dca2c2e04dd512021d3
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-20 16:59:28 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,10 @@
 
 
 class Artist
-    extend Memorable::ClassMethods
+  extend Memorable::ClassMethods
+extend Findable::ClassMethods
+include Memorable::InstanceMethods
+include Paramable::InstanceMethods
 
   attr_accessor :name
   attr_reader :songs
