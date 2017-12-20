14ea397a8cb422444f8ab7d38410f03f545b090c
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-20 16:34:09 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class Artist
 
-  extend environment :: Memorable 
+  extend environment :: Memorable
   
   attr_accessor :name
   attr_reader :songs
@@ -10,27 +10,27 @@ class Artist
   @@artists = [] #empty array of artists
 
   def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name} #can find an artist by name 
+    @@artists.detect{|a| a.name == name} #can find an artist by name
   end
 
   def initialize
-    @@artists << self #adds itself to artist array 
-    @songs = [] #empty array of songs on initalization 
+    @@artists << self #adds itself to artist array
+    @songs = [] #empty array of songs on initalization
   end
 
   def self.all
-    @@artists #knows about all artists 
+    @@artists #knows about all artists
   end
 
 
 
   def add_song(song)
-    @songs << song #take an argument of song and add to array 
+    @songs << song #take an argument of song and add to array
     song.artist = self #song has an artist
   end
 
-  def add_songs(songs) 
-    songs.each { |song| add_song(song) } #this im not sure about 
+  def add_songs(songs)
+    songs.each { |song| add_song(song) } #this im not sure about
   end
 
   def to_param
