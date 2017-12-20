c0ccb133451f348095bf20f3fd4d38ee6267c7c7
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-20 16:43:46 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,15 +1,15 @@
 
      module Memorable
-       module ClassMethods
+       module ClassMethods # i am definining class methods 
          def reset_all
-           self.all.clear
+           self.all.clear # both artist and song can delete everything 
          end
 
          def count
-           self.all.count
+           self.all.count #both artist and song can count how many of themselves they have
          end
        end
-       
+
        module InstanceMethods
          def initialize
            self.class.all << self
